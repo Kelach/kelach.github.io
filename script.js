516940d789878a0b22a7c4b854cc4c42d008b0c1
kelach <kelechi@utexas.edu>
2023-06-07 16:19:19 -0700

    completed generateCards function

--- script.js
+++ script.js
@@ -73,3 +73,33 @@ let fakeMoviesAPI = {
     "total_pages": 98,
     "total_results": 1951
 }
+
+function generateCards(movieObject){
+    // create star
+    let star = document.createElement("span");
+    star.classList.add("star");
+    let starContent = document.createTextNode("⭐️");
+    star.appendChild(starContent);
+
+
+    // create rating
+    let rating = document.createElement("span");
+    let ratingContent = document.createTextNode(movieObject.vote_average);
+    rating.classList.add("rating");
+    rating.appendChild(ratingContent);
+
+
+    // create average container
+    let averageContainer = document.createElement("div");
+    averageContainer.classList.add("average");
+    averageContainer.appendChild(star);
+    averageContainer.appendChild(rating);
+    document.body.appendChild(averageContainer);
+
+    // create image
+    let image = document.createElement("img");
+    image.src = "https://image.tmdb.org/t/p/w342" + movieObject.poster_path;
+    document.body.insertBefore(image, averageContainer);
+
+}
+generateCards(fakeMoviesAPI.results[0]);
